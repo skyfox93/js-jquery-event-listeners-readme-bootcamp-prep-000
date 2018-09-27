33d1fd9f79d47564f2098dc532ca4d6aafe19050
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-27 23:47:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,8 @@
 //define functions here
-
+function getIt(){$('p').on('click', function(){alert("Hey!");})}
+function frameIt(){$('img').on('load', function(){document.querySelector('img').classList.add('tasty');})}
+function pressIt(){$('input').on('keydown', function(event){if(event.which==71){alert();}})}
+function submitIt(){$('form').on('submit', function(){alert('Your form is going to be submitted now.');})}
 $(document).ready(function(){
 
 // call functions here
