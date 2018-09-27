a2aa668fc77d0b8ea3295441a5ee1143bcdc00cf
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-27 23:36:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,6 @@
 //define functions here
 function getIt(){$('p').on('click', function(){alert("Hey!");})}
-function frameIt(){$('img').on('load', function(){$('img').style.classNames.push('tasty');})}
+function frameIt(){$('img').on('load', function(){$('img').classList.add('tasty');})}
 function pressIt(){$('input').on('keydown',function(){if(event.key==="G"){alert();}})}
 
 $(document).ready(function(){
