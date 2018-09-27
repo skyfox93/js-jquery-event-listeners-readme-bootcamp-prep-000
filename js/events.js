6578f48c140234f74929ef18d8518df6167ab7a8
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-27 23:46:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 //define functions here
 function getIt(){$('p').on('click', function(){alert("Hey!");})}
 function frameIt(){$('img').on('load', function(){document.querySelector('img').classList.add('tasty');})}
-function pressIt(){$('input').on('keydown', function(event){if(event.which==="g"){alert();}})}
+function pressIt(){$('input').on('keydown', function(event){if(event.which==){alert();}})}
 function submitIt(){$('form').on('submit', function(){alert('Your form is going to be submitted now.');})}
 $(document).ready(function(){
 
